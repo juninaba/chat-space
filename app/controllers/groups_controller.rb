45cd4d8c3e5da4254eb7b0b820ccae6b3525ef5f
pmo23 <peaceminusone23@yahoo.co.jp>
2019-01-07 14:55:57 +0900

    Fix controller fails

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,22 @@
 class GroupsController < ApplicationController
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def group_params
+
+  end
+
 end
