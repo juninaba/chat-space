41add37cfa28a093b58ffef60f53b925d84d1ba3
pmo23 <peaceminusone23@yahoo.co.jp>
2019-01-08 22:01:32 +0900

    Add action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,19 @@
 class MessagesController < ApplicationController
+  before_action :set_group
 
   def index
+  end
+
+  def create
+  end
+
+  private
+
+  def message_params
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
