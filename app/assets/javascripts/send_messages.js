11ebfcad2f2f876164b997234de3ebd6c1a74d89
pmo23 <peaceminusone23@yahoo.co.jp>
2019-01-16 15:24:20 +0900

    Change val to reset

--- app/assets/javascripts/send_messages.js
+++ app/assets/javascripts/send_messages.js
@@ -39,8 +39,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__message').val('');
-      $('.hidden').val('');
+      $('.new_message')[0].reset();
       $('.form__submit').attr('disabled', false);
       scroll();
     })
